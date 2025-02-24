7327791cfce8b7b681fea91108b694aeea7ad114
Marslalala <108688341+Marslalala@users.noreply.github.com>
2025-02-24 02:15:08 +0000

    Deploying to gh-pages from @ Marslalala/Marslalala.github.io@a207a23e8bf5f9338e3f54006a2fde1d351a8148 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -3,8 +3,8 @@ const ninja = document.querySelector('ninja-keys');
 
 // add the home and posts menu items
 ninja.data = [{
-    id: "nav-about",
-    title: "about",
+    id: "nav-biography",
+    title: "Biography",
     section: "Navigation",
     handler: () => {
       window.location.href = "/";
